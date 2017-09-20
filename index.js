f7a34cd1d53d1340492cc2177f801532644d2b21
Brett Adamson <adamsonb12@gmail.com>
2017-09-20 12:08:53 -0600

    testing index

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({hi: 'there'});
-});
-
-app.get('/best', (req, res) => {
   res.send({go: 'BYU'});
 });
 
